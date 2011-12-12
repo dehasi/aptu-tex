e4a177513de4ccd98a9c55f5895d03aa208fa4c9
dmitriybandurin <dmitriybandurin@gmail.com>
2011-12-13 02:59:25 +0300

    fix BalancedTree.java (previous version is wrong)

--- algorithms/labs/lab2/A/BalancedTree.java
+++ algorithms/labs/lab2/A/BalancedTree.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Pattern;
+import java.util.StringTokenizer;
 
 /**
  * User: Dmitriy Bandurin
@@ -11,20 +11,35 @@ public class BalancedTree {
     public static void main(String[] args) throws IOException {
         InputStreamReader in = new InputStreamReader(System.in);
         BufferedReader bufferedReader = new BufferedReader(in);
-        Pattern spliter = Pattern.compile(" ");
-        final String nodes = bufferedReader.readLine();
-        String[] list = spliter.split(nodes);
-        Node tree = new Node(null, Integer.parseInt(list[0]));
-        for (int i = 1; i < list.length; i++) {
-            final int value = Integer.parseInt(list[i]);
-            if(value != 0){
-                tree.add(value);
-            } else {
-                break;
-            }
+        char[] temp = new char[1024];
+        int readCount;
+        StringBuilder builder = new StringBuilder();
+        while ((readCount = bufferedReader.read(temp)) != -1) {
+            builder.append(temp, 0, readCount);
         }
-        System.out.println(tree.height);
+        final String nodes = builder.toString();
+        StringTokenizer tokenizer = new StringTokenizer(nodes);
+        if (tokenizer.hasMoreTokens()) {
+            Node tree = new Node(null, Integer.parseInt(tokenizer.nextToken()));
+
+            while (tokenizer.hasMoreTokens()) {
+                final int value = Integer.parseInt(tokenizer.nextToken());
+                if (value != 0) {
+                    tree.add(value);
+                } else {
+                    break;
+                }
+            }
+            System.out.println(tree.balanced ? "YES" : "NO");
+        } else {
+            System.out.println(0);
+        }
+
     }
+}
+
+class Counter {
+    static int count = 0;
 }
 
 class Node {
@@ -33,31 +48,34 @@ class Node {
     public Node right;
     public int height;
     public int value;
+    public boolean balanced;
 
     Node(Node parent, int value) {
         this.parent = parent;
         this.value = value;
-        if (value == 0){
+        this.balanced = true;
+        Counter.count++;
+        if (value == 0) {
             this.height = 0;
         } else {
             this.height = 1;
         }
     }
 
-    public void add(int value){
-        if(this.value == value){
+    public void add(int value) {
+        if (this.value == value) {
             return;
         }
 
-        if(this.value < value){
-            if(this.right != null){
+        if (value > this.value) {
+            if (this.right != null) {
                 this.right.add(value);
             } else {
                 this.right = new Node(this, value);
                 this.update();
             }
         } else {
-            if(this.left != null){
+            if (this.left != null) {
                 this.left.add(value);
             } else {
                 this.left = new Node(this, value);
@@ -67,16 +85,14 @@ class Node {
     }
 
     private void update() {
-        int height = this.height;
-        if(this.left != null && this.height < this.left.height + 1){
-            this.height = this.left.height + 1;
-        }
+        int lHeight = this.left != null ? this.left.height : 0;
+        int rHeight = this.right != null ? this.right.height : 0;
+        boolean lBalanced = this.left == null || this.left.balanced;
+        boolean rBalanced = this.right == null || this.right.balanced;
 
-        if(this.right != null && this.height < this.right.height + 1){
-            this.height = this.right.height + 1;
-        }
-
-        if(height != this.height && this.parent != null){
+        this.height = Math.max(lHeight, rHeight) + 1;
+        this.balanced = lBalanced && rBalanced && (Math.max(lHeight, rHeight) - Math.min(lHeight, rHeight)) < 2;
+        if (this.parent != null) {
             this.parent.update();
         }
     }
